f7a1188283267a9acdbffa974fbff42cae902b64
giampq <giampq.it@gmail.com>
2018-02-27 14:47:14 +0700

    empty project

--- app/bundle/programs/server/npm/node_modules/mac-regex/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function(options) {
-  options = options || {};
-  var regexBase = '([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})';
-
-  return options.exact ? new RegExp('^' + regexBase + '$') :
-                         new RegExp(regexBase, 'g');
-}
